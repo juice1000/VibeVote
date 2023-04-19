cd80c6e68df387a6d6ba52e4da4df1a3bf0dc172
Idar <idarnigatu@gmail.com>
2023-04-19 18:57:49 +0200

    feat: Initialized spotify web api

---
+++ backend/src/config/spotify.ts
@@ -0,0 +1,10 @@
+import SpotifyWebApi from 'spotify-web-api-node';
+
+const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } = process.env;
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: SPOTIFY_CLIENT_ID,
+  clientSecret: SPOTIFY_CLIENT_SECRET,
+});
+
+export default spotifyApi;
