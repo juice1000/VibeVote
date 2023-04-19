267ac2c22960e3942b6aa39d9c94a3e03cdc689e
Idar <idarnigatu@gmail.com>
2023-04-19 19:21:04 +0200

    feat: created route for creating playlist

--- backend/src/routes/playlist.ts
+++ backend/src/routes/playlist.ts
@@ -1,10 +1,26 @@
 import express from 'express';
+import { Prisma, PrismaClient } from '@prisma/client';
+import spotifyApi from '../config/spotify';
 
+const prisma = new PrismaClient();
 const router = express.Router();
+console.log(prisma.playlist);
 
 // Define the API routes related to playlist management
-router.post('/create', (req, res) => {
-  // Handle playlist creation
+router.post('/create', async (req, res) => {
+  try {
+    const { title, description } = req.body;
+
+    const newPlaylist = await prisma.playlist.create({
+      data: {
+        title,
+        description,
+      },
+    });
+    res.status(201).json(newPlaylist);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 router.post('/:playlistId/add-track', (req, res) => {
