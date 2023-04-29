0f7938f4b726006220a31d8b513b172d39981caa
Deftool66 <deftool@gmx.net>
2023-04-29 10:04:44 +0200

    created a test for the home component

--- frontend/src/app/components/home/home.component.spec.ts
+++ frontend/src/app/components/home/home.component.spec.ts
@@ -1,23 +1,76 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
+import { PlaylistService } from 'src/app/services/playlist.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
-describe('HomeComponent', () => {
+class MockPlayListService extends PlaylistService {
+  createPlayList() {
+    return { spotifyPlayListId: '1234' };
+  }
+}
+fdescribe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let testBedService: PlaylistService;
+  // let componentService: PlaylistService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      declarations: [HomeComponent],
+      imports: [HttpClientModule, RouterTestingModule],
+      providers: [PlaylistService],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+    }).compileComponents();
+    TestBed.overrideComponent(HomeComponent, {
+      set: {
+        providers: [
+          { provide: PlaylistService, useClass: MockPlayListService },
+        ],
+      },
+    });
+    // AuthService provided to the TestBed
+    testBedService = TestBed.get(PlaylistService);
+
+    // AuthService provided by Component, (should return MockAuthService)
+    // componentService = fixture.debugElement.injector.get(PlaylistService);
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Service injected via inject(...) and TestBed.get(...) should be the same instance', inject(
+    [PlaylistService],
+    (injectService: PlaylistService) => {
+      expect(injectService).toBe(testBedService);
+    }
+  ));
+
+  it('Component should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Component should have a name', () => {
+    expect(component.newPlaylistName).toBe('');
+  });
+
+  it('Component should have a childFriendly', () => {
+    expect(component.childFriendly).toBeFalsy();
+  });
+
+  it('should create', () => {
+    expect(component.createPlaylist).toBeTruthy();
+  });
+  // it('should create playlist', (done) => {
+  //   let object = { spotifyPlayListId: '1234' };
+  //   spyOn(component, 'createPlaylist');
+  //   expect(component.createPlaylist).toHaveBeenCalledWith();
+
+  //   // component.createPlaylist();
+  //   // let result = await testBedService.createPlaylist('name', false);
+  //   // expect(result.toEqual({ spotifyPlayListId: '1234' }));
+  //   // expect
+  // });
 });
--- frontend/src/app/components/login/login.component.spec.ts
+++ frontend/src/app/components/login/login.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+// import { TestComponent } from '../test/test.component';
 
 import { LoginComponent } from './login.component';
 
@@ -8,9 +11,25 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      declarations: [LoginComponent],
+      imports: [Router, AuthService],
+      providers: [Router, AuthService],
+      // providers: [AuthService]
+      // providers: [Router]
+      // providers: [TestComponent]
+      // providers: [TestBed]
+
+      // providers: [TestBed]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    await fixture.whenStable();
+    await component.joinSession();
+    await component.loginWithSpotify();
+
+    console.log(component.sessionId);
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
