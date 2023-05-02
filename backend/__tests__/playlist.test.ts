9d098c357863f683ff11d3d44c8c18c2db2fbe15
juice1000 <julienlook@gmx.de>
2023-05-02 12:32:55 +0200

    comments for playlists tests

--- backend/__tests__/playlist.test.ts
+++ backend/__tests__/playlist.test.ts
@@ -1,56 +1,36 @@
-import { socketHandler, checkConnection } from '../src/io-service';
-jest.mock('../src/io-service'); // that one imports index.ts and seems to reinitialize app which we don't want
-(checkConnection as jest.Mock).mockResolvedValue({});
-(socketHandler as jest.Mock).mockResolvedValue({});
+// import { socketHandler, checkConnection } from '../src/io-service';
+// jest.mock('../src/io-service'); // that one imports index.ts and seems to reinitialize app which we don't want
+// (checkConnection as jest.Mock).mockResolvedValue({});
+// (socketHandler as jest.Mock).mockResolvedValue({}); // we want to mock socket function too, although opinions about that seem to be divided
 
 import request from 'supertest';
-import { app, server } from '@root'; // server already used by other test files
+import { app, server, io } from '@root'; // server already used by other test files and sometimes not properly closing, need to investigate on that
 
 import { prismaMock } from './singleton';
 import * as mocks from './__mocks__/playlist-data';
-
-// describe('App GET /api/playlist/:spotifyPlaylistId', () => {
-//   const mockPlaylist = {
-//     data: {
-//       title: 'title',
-//       description: 'description',
-//       spotifyPlaylistId: 'spotifyPlaylistId',
-//       childFriendly: true,
-//     },
-//   };
-//   beforeAll((done) => {
-//     done();
-//   });
-//   afterAll((done) => {
-//     server.close();
-//     done();
-//   });
-//   it('returns status code 200 when server available', async () => {
-//     app.post('/api/playlist/create');
-
-//     const res = await request(app).get('/api/playlist/:spotifyPlaylistId');
-//     expect(res.statusCode).toEqual(200);
-//   });
-// });
 
 describe('App POST /api/playlist/:spotifyPlaylistId', () => {
   beforeAll((done) => {
     done();
   });
+
   afterAll((done) => {
+    io.close();
     server.close();
     done();
   });
 
-  // we should mock each szenario where failures could happen
+  // we should also mock szenarios where failures could happen
   const data: any = {
     title: 'title',
     description: 'description',
-    spotifyPlaylistId: 'spotifyPlaylistId3', // it's unique so we have to create a new one if we don't mock the function it
+    spotifyPlaylistId: 'spotifyPlaylistId14', // it's unique so we have to create a new one if we don't mock the function it
     childFriendly: true,
   };
   prismaMock.playlist.create.mockResolvedValue(mocks.playList);
+  //prismaMock.playlist.delete.mockResolvedValue(mocks.playList);
 
+  // testing on database only first, then with server
   it('should create new playlist ', async () => {
     expect(
       prismaMock.playlist.create({
@@ -60,14 +40,7 @@ describe('App POST /api/playlist/:spotifyPlaylistId', () => {
   });
 
   it.only('should create new playlist on endpoint /create', async () => {
-    // we should mock each szenario where failures could happen
-    //(socketHandler as jest.Mock).mockResolvedValue({});
-    // (checkConnection as jest.Mock).mockResolvedValue({});
-
     const resSuccessfulRequest = await request(app).post('/api/playlist/create').send(data);
     expect(resSuccessfulRequest.statusCode).toEqual(201);
-
-    //const resBadRequest = await request(app).post('/auth/refresh').send({});
-    //expect(resBadRequest.statusCode).toEqual(400);
   });
 });
