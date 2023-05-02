9b99c71ea8cedb5859b41226c965f8f19ccc80de
Deftool66 <deftool@gmx.net>
2023-05-02 09:11:11 +0200

    added test to the login component

--- frontend/src/app/components/login/login.component.spec.ts
+++ frontend/src/app/components/login/login.component.spec.ts
@@ -1,42 +1,58 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
-// import { TestComponent } from '../test/test.component';
-
 import { LoginComponent } from './login.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+
+class MockAuthService extends AuthService {
+  joinSession() {
+    return { spotifyPlayListId: '1234' };
+  }
+}
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let testBedService: AuthService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      imports: [Router, AuthService],
-      providers: [Router, AuthService],
-      // providers: [AuthService]
-      // providers: [Router]
-      // providers: [TestComponent]
-      // providers: [TestBed]
-
-      // providers: [TestBed]
+      imports: [HttpClientModule, RouterTestingModule],
+      providers: [AuthService],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
     }).compileComponents();
-
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-    await fixture.whenStable();
-    await component.joinSession();
-    await component.loginWithSpotify();
-
-    console.log(component.sessionId);
+    TestBed.overrideComponent(LoginComponent, {
+      set: {
+        providers: [{ provide: AuthService, useClass: MockAuthService }],
+      },
+    });
+    testBedService = TestBed.get(AuthService);
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Service injected via inject(...) and TestBed.get(...) should be the same instance', inject(
+    [AuthService],
+    (injectService: AuthService) => {
+      expect(injectService).toBe(testBedService);
+    }
+  ));
+
+  it('Component should be created', () => {
+    expect(component.joinSession).toBeTruthy();
+  });
+  it('Component should have a sessionId', () => {
+    expect(component.sessionId).toBeUndefined();
+  });
+  it('Component should have a loginWithSpotify', () => {
+    expect(component.loginWithSpotify).toBeTruthy();
+  });
+  it('Component should have a joinSession', () => {
+    expect(component.joinSession).toBeTruthy();
   });
 });
