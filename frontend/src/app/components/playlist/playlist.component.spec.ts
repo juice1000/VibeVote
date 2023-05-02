a8879792c6732b97ccecd25062847c472eae6985
Deftool66 <deftool@gmx.net>
2023-05-02 09:12:20 +0200

    added test to the playlist component

--- frontend/src/app/components/playlist/playlist.component.spec.ts
+++ frontend/src/app/components/playlist/playlist.component.spec.ts
@@ -1,18 +1,53 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlaylistComponent } from './playlist.component';
+import { PlaylistService } from 'src/app/services/playlist.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
+class MockPlayListService extends PlaylistService {
+  createPlayList() {
+    return {
+      childFriendly: true,
+      createdAt: '2023-05-01T14:02:15.255Z',
+      description: null,
+      id: 11,
+      spotifyAccessToken: null,
+      spotifyPlaylistId: '6fhNCRL0G73vnEJ88GET81',
+      spotifyRefreshToken: null,
+      spotifyTokenExpiresAt: null,
+      title: 'test',
+      updatedAt: '2023-05-01T14:02:15.255Z',
+    };
+  }
+}
 describe('PlaylistComponent', () => {
   let component: PlaylistComponent;
   let fixture: ComponentFixture<PlaylistComponent>;
+  let testBedService: PlaylistService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PlaylistComponent ]
-    })
-    .compileComponents();
+      declarations: [PlaylistComponent],
+      imports: [RouterTestingModule, HttpClientModule, DebugElement],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+      providers: [MockPlayListService],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(PlaylistComponent);
+    TestBed.overrideComponent(PlaylistComponent, {
+      set: {
+        providers: [
+          {
+            provide: 'playlistService',
+            useClass: MockPlayListService,
+          },
+        ],
+      },
+    });
+    fixture = await TestBed.createComponent(PlaylistComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
