e3e3f8cbe90a6d6a82ea24a7db28545047e197f1
juice1000 <julienlook@gmx.de>
2023-05-03 16:24:13 +0200

    click login button

--- frontend/cypress/e2e/vibeVote.cy.ts
+++ frontend/cypress/e2e/vibeVote.cy.ts
@@ -1,5 +1,11 @@
-it('should navigate to the home page', () => {
-  cy.visit('http://localhost:4200');
-  cy.get('#sessionId').contains()
-
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
+describe('Cypress login route', () => {
+  it('should navigate to the login', () => {
+    cy.visit('http://localhost:4200');
+    cy.get('#loginWithSpotify').click();
+  });
 });
