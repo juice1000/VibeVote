697a4a2f81f07484845b7481de24ac16e64363b3
juice1000 <julienlook@gmx.de>
2023-05-04 10:35:26 +0200

    cleanup commands

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -1,49 +1,3 @@
-//@ts-nocheck
-
-// ***********************************************
-// This example namespace declaration will help
-// with Intellisense and code completion in your
-// IDE or Text Editor.
-// ***********************************************
-// declare namespace Cypress {
-//   interface Chainable<Subject = any> {
-//     login(param: any): typeof login;
-//   }
-// }
-//
-// function customCommand(param: any): void {
-//   console.warn(param);
-// }
-//
-// NOTE: You can use it like so:
-// Cypress.Commands.add('customCommand', customCommand);
-//
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-
 declare global {
   namespace Cypress {
     interface Chainable<Subject = any> {
@@ -52,21 +6,6 @@ declare global {
     }
   }
 }
-
-// const options = {
-//   method: "POST",
-//   url: Cypress.env("authUrl"),
-//   body: {
-//     grant_type: "password",
-//     username: Cypress.env("username"),
-//     password: Cypress.env("password"),
-//     //   audience: Cypress.env('auth_audience'),
-//     //   scope: 'openid profile email',
-//     //   client_id: Cypress.env('auth_client_id'),
-//     //   client_secret: Cypress.env('auth_client_secret'),
-//   },
-// };
-// cy.request(options);
 
 Cypress.Commands.add('login', () => {
   Cypress.log({
