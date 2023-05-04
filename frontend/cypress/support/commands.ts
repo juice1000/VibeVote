1dfcbb2091b6d78e6505fc4791a9cb3a112e6cec
juice1000 <julienlook@gmx.de>
2023-05-04 10:34:54 +0200

    make custom commands work

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -48,6 +48,7 @@ declare global {
   namespace Cypress {
     interface Chainable<Subject = any> {
       login(): void;
+      addSong(): void;
     }
   }
 }
@@ -71,6 +72,23 @@ Cypress.Commands.add('login', () => {
   Cypress.log({
     name: 'loginViaAuth0',
   });
+});
+
+Cypress.Commands.add('addSong', () => {
+  cy.get('[data-cy="partyInput"]').type('6oZDCanh1AozI2LdFG0Cbj');
+  cy.get('.mt-2').click();
+  cy.get('.add-track-btn').click();
+  cy.get('.relative > .w-full').type('Tool The Pot{enter}');
+  cy.wait(500);
+  cy.get('.absolute > :nth-child(1)').click();
+  // cy.get('[data-cy="card"]')
+  //   .find('[data-cy="trackTitle"]')
+  //   .then((items) => {
+  //     const list = Array.from(items, (item) => item.innerText.toLowerCase());
+  //     console.log(list);
+  //     expect(list).to.include('tool');
+  //     expect(list).to.include('the pot');
+  //   });
 });
 
 export {};
