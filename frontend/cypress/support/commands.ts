f64fc6be0004b777f0363f66120643ab4c140dda
juice1000 <julienlook@gmx.de>
2023-05-04 12:07:14 +0200

    custom commands, update vote for login route

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -2,7 +2,9 @@ declare global {
   namespace Cypress {
     interface Chainable<Subject = any> {
       login(): void;
-      addSong(): void;
+      addTrack(): void;
+      vote(): void;
+      play(): void;
     }
   }
 }
@@ -13,21 +15,31 @@ Cypress.Commands.add('login', () => {
   });
 });
 
-Cypress.Commands.add('addSong', () => {
-  cy.get('[data-cy="partyInput"]').type('6oZDCanh1AozI2LdFG0Cbj');
-  cy.get('.mt-2').click();
+Cypress.Commands.add('addTrack', () => {
   cy.get('.add-track-btn').click();
   cy.get('.relative > .w-full').type('Tool The Pot{enter}');
-  cy.wait(500);
   cy.get('.absolute > :nth-child(1)').click();
-  // cy.get('[data-cy="card"]')
-  //   .find('[data-cy="trackTitle"]')
-  //   .then((items) => {
-  //     const list = Array.from(items, (item) => item.innerText.toLowerCase());
-  //     console.log(list);
-  //     expect(list).to.include('tool');
-  //     expect(list).to.include('the pot');
-  //   });
+});
+
+Cypress.Commands.add('vote', () => {
+  cy.get(
+    'body > app-root > app-playlist > div > div:nth-child(2) > div > div > div > button'
+  )
+    .then(function ($elem) {
+      expect($elem.text()).to.equal(' Vote (0) ');
+    })
+    .click();
+
+  cy.wait(500);
+  cy.get(
+    'body > app-root > app-playlist > div > div:nth-child(2) > div > div > div > button'
+  ).then(function ($elem) {
+    expect($elem.text()).to.equal(' Vote (1) ');
+  });
+});
+
+Cypress.Commands.add('play', () => {
+  cy.get('#play-button').click();
 });
 
 export {};
